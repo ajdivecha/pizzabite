50c48db6a6952f5cc400e62594f7a36a87b44f97
Adrian Divecha <ajdivecha@macbook-pro-2.home>
2017-12-10 20:24:15 -0800

    config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 var mysql = require("mysql");
 
-
+if (process.env.JAWSDB_URL){
+  connection = mySQL.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "pizzaDB"
 });
+}
 
 connection.connect(function(err) {
  if (err) {
